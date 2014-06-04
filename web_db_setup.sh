8454686c9d5bd9590a2663217cca2831580556bf
rrignier <raphael@rignier.com>
2014-06-04 20:09:13 +0200

    -Add GPL in source file. -add rename state after clone

--- web_db_setup.sh
+++ web_db_setup.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# Copyright 2014 R. RIGNIER
+#This file is part of ltsp-osdeploy.
+#
+#    Foobar is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    ltsp-osdeploy is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with ltsp-osdeploy.  If not, see <http://www.gnu.org/licenses/>.
+ 
 #Install Web Server and Database for Pyddlaj
 
 echo "Installing Web Server"
